185b8e4e72c54ffe874d1477ca1caa9d684d5a83
Vicandujar111 <vicandujar@yahoo.com>
2024-02-13 12:15:36 -0500

    updated

--- Starter_Code/Instructions/Starter_Code/pychain.py
+++ Starter_Code/Instructions/Starter_Code/pychain.py
@@ -51,9 +51,9 @@ import hashlib
 # `amount` attributes
 @dataclass
 class Record:
-    sender = str
-    receiver = str
-    amount = float
+    sender: str
+    receiver: str
+    amount: float
 
 
 ################################################################################
@@ -193,7 +193,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        record=input_data,
+        record=Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
